1799b98e8178dd6c6ef8c69a7299d28a27bf8b62
nanarama <annasampers@gmail.com>
2022-11-18 14:37:30 -0600

    Add all RESTful methods to KittensController

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,53 @@
+class KittensController < ApplicationController
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:notice] = 'You added a new kitten!'
+      redirect_to kitten_path(params[:id])
+    else  
+      render :new, status: :unprocessable_entity
+    end  
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: "Kitten has been updated."
+    else  
+      render :edit, status: :unprocessable_entity
+    end    
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to root_path, notice: "Kitten was successfully destroyed."
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+
+end
